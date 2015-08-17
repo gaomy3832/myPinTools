1124743ad349edaa26d0eef513f788281efe219e
Mingyu Gao <mgao12@stanford.edu>
2015-08-17 16:13:14 -0700

    [hooks] Avoid name mangling of hook interface.
    Use "extern C" to avoid name mangling in C++. See
    https://en.wikipedia.org/wiki/Name_mangling#Handling_of_C_symbols_when_linking_from_C.2B.2B

--- hooks/pintool_hooks.h
+++ hooks/pintool_hooks.h
@@ -1,9 +1,17 @@
 #ifndef PINTOOL_HOOKS_H_
 #define PINTOOL_HOOKS_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define ATTR_HOOK_FUNC __attribute__((externally_visible,noinline))
 
 void pintool_roi_begin() ATTR_HOOK_FUNC;
 void pintool_roi_end() ATTR_HOOK_FUNC;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // PINTOOL_HOOKS_H_
